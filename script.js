dc332623b148e10fafcb8753737f6c79677bfbc0
Tom Mitchell <mitcht032@gmail.com>
2020-08-23 08:23:42 -0700

    added comments noticed some functions aren't working

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-
+//Start button prompt
 
 document.getElementById("start").addEventListener("click", function(){
 
     document.getElementById("prompt")
 })
+//generate a lowercase password
+function getLower(){
+ 
+    
+}
+
 //generate a random password
 function generate(){
 
     //setting password length and variation
-    var variation = document.getElementById("lengthPassword").value;
+    let variation = document.getElementById("lengthPassword").value;
 
-    //might need to change the "slider" into the user input for length.
 
-    //might need to make an array to hold all the var's or choices
+    //might need to make an array to hold all the var's or choices?
 
     //possible password value
-    var allSymbols ="123456789!#$%&'()*+,-./:;<=>?@[]`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerPassword ="abcdefghijklmnopqrstuvwxyz";
-    var upperPassword ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var symPassord = "!#$%&'()*+,-./:;<=>?@[]`{|}~";
-    var numPassword ="123456789";
-    var password ="";
+    let allSymbols ="123456789!#$%&'()*+,-./:;<=>?@[]`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowerPassword ="abcdefghijklmnopqrstuvwxyz";
+    //var upperPassword ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //var symPassord = "!#$%&'()*+,-./:;<=>?@[]`{|}~";
+    //var numPassword ="123456789";
+    let password ="";
 
     //loop to choose password charcters for allSymbols
     for(var i = 1; i <= variation; i++){
-        password = password + allSymbols.charAt(Math.floor(Math.random() * Math.floor(allSymbols.length -1))); 
+        password = password + allSymbols.(Math.floor(Math.random() * Math.floor(allSymbols.length -1))); 
     }
     //loop to choose password charcters for lowerPassword
     
-    
-    document.getElementById("lengthPassword").value = password;
+
+    //for(var i= 1; i <= lowPassword; i++){
+        //password = password + lowerPassword.charAt(Math.floor(Math.random() * Math.floor(lowerPassword.length -1)));     }
+    //document.getElementsByClassName("PasswordChoice").value = password;
+    //document.getElementById("lengthPassword").value = password;
     //password to display area
     document.getElementById("display").value = password;
 }
