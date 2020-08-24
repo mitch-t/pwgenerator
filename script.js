f7188ce2c09cf08a56397443e9adc2199167be71
Tom Mitchell <66184450+mitch-t@users.noreply.github.com>
2020-08-23 22:30:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -21,21 +21,21 @@ function generate(){
 
     //possible password value
     var allSymbols ="123456789!#$%&'()*+,-./:;<=>?@[]`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerPassword ="abcdefghijklmnopqrstuvwxyz";
+    //var lowerPassword ="abcdefghijklmnopqrstuvwxyz";
     //var upperPassword ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     //var symPassord = "!#$%&'()*+,-./:;<=>?@[]`{|}~";
     //var numPassword ="123456789";
     var password ="";
 
     //loop to choose password charcters for allSymbols
-    for(var i = 1; i <= variation; i++){
-        password = password + allSymbols.charAt(Math.floor(Math.random() * Math.floor(allSymbols.length -1))); 
+    for(var i = 0; i < variation; i++){
+        password = password + allSymbols.charAt(Math.random() * (allSymbols.length -1)); 
     }
-    //loop to choose password charcters for lowerPassword
+    //loop to choose password charcters for lowerPassword - DOESN"T WORK
     
 
-    //for(var i= 1; i <= lowPassword; i++){
-        //password = password + lowerPassword.charAt(Math.floor(Math.random() * Math.floor(lowerPassword.length -1)));     }
+    //for(var i= 0; i < lowPassword; i++){
+        //password = password + lowerPassword.charAt(Math.random() * Math.floor(lowerPassword.length -1));     }
     //document.getElementsByClassName("PasswordChoice").value = password;
     //document.getElementById("lengthPassword").value = password;
     //password to display area
